3808524c716cc90f8c7c6cef817c414b207fea2d
Dean Tawonezvi <deantawonezvi1@gmail.com>
2024-05-09 00:25:40 +0200

    Adds initial app skeleton

--- src/App.js
+++ src/App.js
@@ -1,37 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
+  Button,
+  ChakraProvider,
+  Checkbox,
+  extendTheme,
   Grid,
-  theme,
+  Slider, SliderFilledTrack, SliderThumb,
+  SliderTrack,
+  Text,
+  VStack,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import './App.css';
+import { FaRegCopy } from 'react-icons/fa';
+import { IoMdArrowForward } from 'react-icons/io';
+
+const theme = extendTheme({
+  colors: {
+    teal: {
+      500: '#a4ffaf',
+    },
+  },
+});
 
 function App() {
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+      <Box textAlign="center" fontSize="xl" bg={'#131218'}>
+        <Grid minH="100vh" p={3} placeItems="center">
+
+          <VStack spacing={8} width={["100%", "80%", "60%", "40%", "30%"]}  align="start">
+            <Box width="100%" textAlign="center">
+              <Text fontSize='2xl' color={'#82808d'} fontFamily={'JetBrainsMono Bold'}>Password Generator</Text>
+            </Box>
+            <Box bg={'#24232b'} w='100%' p={4} color='white' fontFamily={'JetBrainsMono Bold'}>
+              <FaRegCopy />
+            </Box>
+            <VStack bg={'#24232b'} w="100%" p={4} color="white" align="start" padding={5}>
+              <br />
+              <Slider aria-label="slider-ex-1" colorScheme='teal' defaultValue={30}>
+                <SliderTrack bg={'black'}>
+                  <SliderFilledTrack />
+                </SliderTrack>
+                <SliderThumb />
+              </Slider>
+              <br />
+              <Checkbox colorScheme="green" color="white"
+                        fontFamily={'JetBrainsMono Bold'} paddingY={1}>Include Uppercase letters</Checkbox>
+              <Checkbox colorScheme="green"  color="white"
+                        fontFamily={'JetBrainsMono Bold'} paddingY={1}>Include Lowercase letters</Checkbox>
+              <Checkbox colorScheme="green" color="white"
+                        fontFamily={'JetBrainsMono Bold'} paddingY={1}>Include Numbers</Checkbox>
+              <Checkbox colorScheme="green" color="white"
+                        fontFamily={'JetBrainsMono Bold'} paddingY={1}>Include Symbols</Checkbox>
+              <br />
+              <Button rightIcon={<IoMdArrowForward />} color={'black'} colorScheme="teal" width="100%"
+                      fontFamily={'JetBrainsMono Bold'} borderRadius={0} padding={7} _hover={{ bg: 'teal.500' }}>
+                GENERATE
+              </Button>
+            </VStack>
+
           </VStack>
         </Grid>
       </Box>
