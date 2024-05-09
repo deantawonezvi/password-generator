9d0f7aaa6c82d316091ee905ff74768bb17ce7b6
Dean Tawonezvi <deantawonezvi1@gmail.com>
2024-05-09 02:43:49 +0200

    Updates tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,59 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-import { render } from './test-utils';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn chakra/i);
-  expect(linkElement).toBeInTheDocument();
+});
+
+test('generates password of correct length', async () => {
+  const { getByText, getByRole } = render(<App />);
+
+  fireEvent.click(getByText('GENERATE'));
+
+  await waitFor(() => {
+    const password = getByRole('textbox');
+    expect(password.value.length).toBe(10);
+  });
+});
+
+test('generates password with correct character types', async () => {
+  const { getByText, getByRole, getByLabelText } = render(<App />);
+
+  fireEvent.click(getByLabelText('Include Uppercase letters'));
+  fireEvent.click(getByLabelText('Include Numbers'));
+  fireEvent.click(getByText('GENERATE'));
+
+  await waitFor(() => {
+    const password = getByRole('textbox');
+    expect(password.value).toMatch(/[A-Z]/);
+    expect(password.value).toMatch(/[0-9]/);
+  });
+});
+
+test('calculates password strength correctly', async () => {
+  const { getByText, getByRole, getByLabelText } = render(<App />);
+
+  fireEvent.click(getByLabelText('Include Uppercase letters'));
+  fireEvent.click(getByLabelText('Include Numbers'));
+  fireEvent.click(getByText('GENERATE'));
+
+  await waitFor(() => {
+    const strength = getByText(/STRENGTH/);
+    expect(strength.textContent).toBe('MEDIUM');
+  });
+});
+
+test('copies password to clipboard', async () => {
+  const { getByText, getByRole } = render(<App />);
+
+  fireEvent.click(getByText('GENERATE'));
+
+  await waitFor(() => {
+    const copyButton = getByRole('button', { name: /Copy to clipboard/ });
+    fireEvent.click(copyButton);
+
+    const copiedText = getByText('Copied!');
+    expect(copiedText).toBeInTheDocument();
+  });
 });
